6c9419c63e310285a9bb0c2352d44781492da95e
wali98-rgb <wyyuddin@gmail.com>
2024-05-11 16:26:50 +0700

    modify: link sass & css

--- pages/public/destination/view.php
+++ pages/public/destination/view.php
@@ -24,7 +24,8 @@
     <!-- Mobile Device -->
     <link rel="stylesheet" href="../../../assets/plugin/css/responsive/mobile/index.scss">
     <!-- Partials Mobile -->
-    <link rel="stylesheet" href="../../../assets/plugin/css/responsive/mobile/partials/navbar.scss">
+    <link rel="stylesheet" href="../../../assets/plugin/css/responsive/mobile/partials/scss/navbar.scss">
+    <link rel="stylesheet" href="../../../assets/plugin/css/responsive/mobile/partials/css/navbar.css">
 
     <!-- My Fav Icon -->
     <link rel="icon" href="../../../assets/img/logo/favicon.jpeg">
